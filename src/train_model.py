6c665a23dde236bf3b772c4cacc633e7459fd40e
Lants Ks <LanK08102001@gmail.com>
2024-05-20 20:29:37 +0300

    Отделена валидация

--- src/train_model.py
+++ src/train_model.py
@@ -3,7 +3,6 @@ import argparse
 import logging
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
 from joblib import dump
 
 logger = logging.getLogger(__name__)
@@ -13,7 +12,6 @@ logging.basicConfig(
     format='%(asctime)s %(message)s')
 
 TRAIN_DATA = 'data/proc/train.csv'
-VAL_DATA = 'data/proc/val.csv'
 MODEL_SAVE_PATH = 'models/linear_regression_v01.joblib'
 
 
@@ -21,9 +19,6 @@ def main(args):
     df_train = pd.read_csv(TRAIN_DATA)
     x_train = df_train[['total_meters']]
     y_train = df_train['price']
-    df_val = pd.read_csv(VAL_DATA)
-    x_val = df_val[['total_meters']]
-    y_val = df_val['price']
 
     linear_model = LinearRegression()
     linear_model.fit(x_train, y_train)
@@ -31,12 +26,10 @@ def main(args):
     logger.info(f'Saved to {args.model}')
 
     r2 = linear_model.score(x_train, y_train)
-    y_pred = linear_model.predict(x_val)
-    mae = mean_absolute_error(y_pred, y_val)
     c = int(linear_model.coef_[0])
     inter = int(linear_model.intercept_)
 
-    logger.info(f'R2 = {r2:.3f}     MAE = {mae:.0f}     Price = {c} * area + {inter}')
+    logger.info(f'R2 = {r2:.3f}  Price = {c} * area + {inter}')
 
 
 if __name__ == '__main__':
