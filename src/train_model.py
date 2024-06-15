71f6c821b064d748f9e5c8228431eb3cd02b5bfd
Lants Ks <LanK08102001@gmail.com>
2024-06-15 14:39:09 +0300

    new model

--- src/train_model.py
+++ src/train_model.py
@@ -4,9 +4,6 @@ import logging
 import pandas as pd
 #from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
 from joblib import dump
 
 logger = logging.getLogger(__name__)
@@ -18,23 +15,12 @@ logging.basicConfig(
 TRAIN_DATA = 'data/proc/train.csv'
 MODEL_SAVE_PATH = 'models/model_rf_BEST.joblib'
 
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('cat', OneHotEncoder(handle_unknown='ignore'), ['author','author_type','url','location','deal_type','accommodation_type','district','street','house_number','underground','residential_complex'])
-    ])
-
-pipeline = Pipeline([
-    ('preprocessor', preprocessor),
-    ('regressor', RandomForestRegressor(n_estimators=100, random_state=42))
-])
-
 
 def main(args):
     df_train = pd.read_csv(TRAIN_DATA)
-    x_train = df_train.drop(['price'], axis=1)
+    x_train = df_train[['floor','floors_count','rooms_count','total_meters']]
     y_train = df_train['price']
-
-    model = pipeline
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
     model.fit(x_train, y_train)
     dump(model, args.model)
     logger.info(f'Saved to {args.model}')
