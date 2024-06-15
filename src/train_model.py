b86f118e8076961d6f77247fed1745063075fda9
Lants Ks <LanK08102001@gmail.com>
2024-06-15 13:25:09 +0300

    add new model

--- src/train_model.py
+++ src/train_model.py
@@ -2,7 +2,11 @@
 import argparse
 import logging
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 from joblib import dump
 
 logger = logging.getLogger(__name__)
@@ -12,25 +16,32 @@ logging.basicConfig(
     format='%(asctime)s %(message)s')
 
 TRAIN_DATA = 'data/proc/train.csv'
-MODEL_SAVE_PATH = 'models/linear_regression_v01.joblib'
+MODEL_SAVE_PATH = 'models/model_rf_BEST.joblib'
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(handle_unknown='ignore'), ['author','author_type','url','location','deal_type','accommodation_type','district','street','house_number','underground','residential_complex'])
+    ])
+
+pipeline = Pipeline([
+    ('preprocessor', preprocessor),
+    ('regressor', RandomForestRegressor(n_estimators=100, random_state=42))
+])
 
 
 def main(args):
     df_train = pd.read_csv(TRAIN_DATA)
-    x_train = df_train[['total_meters']]
+    x_train = df_train.drop(['price'], axis=1)
     y_train = df_train['price']
 
-    linear_model = LinearRegression()
-    linear_model.fit(x_train, y_train)
-    dump(linear_model, args.model)
+    model = pipeline
+    model.fit(x_train, y_train)
+    dump(model, args.model)
     logger.info(f'Saved to {args.model}')
 
-    r2 = linear_model.score(x_train, y_train)
+    r2 = model.score(x_train, y_train)
 
-    c = int(linear_model.coef_[0])
-    inter = int(linear_model.intercept_)
-
-    logger.info(f'R2 = {r2:.3f}  Price = {c} * area + {inter}')
+    logger.info(f'R2 = {r2:.3f} ')
 
 
 if __name__ == '__main__':
