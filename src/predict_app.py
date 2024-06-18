5a4c9614e4429a26fcd9f7f1dace7ec147ad0ed1
Lants Ks <LanK08102001@gmail.com>
2024-06-18 19:06:54 +0300

    add new model

--- src/predict_app.py
+++ src/predict_app.py
@@ -4,26 +4,6 @@ from flask import Flask, request
 from flask_cors import CORS
 from joblib import load
 from flask_httpauth import HTTPTokenAuth
-import time
-import numpy as np
-
-def predict_io_bounded(area):
-    """Emulate io delay"""
-    time.sleep(1)
-    avg_price = 200_000                 # RUB / m2
-    return int(area * avg_price)
-
-
-def predict_cpu_bounded(area, n=5_000_000):
-    """Emulate single thread computation"""
-    avg_price = sum([x for x in range(n)]) / n
-    return int(area * avg_price)
-
-
-def predict_cpu_multithread(area, n=5_000_000):
-    """Emulate multi thread computation"""
-    avg_price = np.mean(np.arange(n))
-    return int(area * avg_price)
 
 
 MODEL_SAVE_PATH = 'models/model_rf_BEST.joblib'
@@ -53,7 +33,7 @@ def predict(in_data: dict) -> int:
     :raise Error: If something goes wrong.
     :return: House price, RUB.
     :rtype: int
-
+    """
     total_meters = float(in_data['total_meters'])
     floor = int(in_data['floor'])
     floors_count = int(in_data['floors_count'])
@@ -66,9 +46,6 @@ def predict(in_data: dict) -> int:
                             total_meters,
                             first_floor,
                             last_floor]])
-    """
-    area = int(in_data['area'])
-    price = predict_cpu_multithread(area, 435_000_000)
     return int(price)
 
 
